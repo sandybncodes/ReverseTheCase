3e735009d5361da77b333dad2efa12b30646c183
sandy <105875909+sandybncodes@users.noreply.github.com>
2022-07-28 11:33:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseTheCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare the string
+            string str = "Hello Alex";
+
+            // declare an array which will store each char from declared string
+            char[] str_to_array = str.ToCharArray();
+
+            // using for loop and if statment reverse case
+            for(int i = 0; i < str_to_array.Length; i++)
+            {
+                // if the char is upper then reverse to lower
+                if(Char.IsUpper(str_to_array[i]) == true)
+                {
+                    str_to_array[i] = Char.ToLower(str_to_array[i]);
+                }
+                // else if the char is lower then reverse to upper
+                else if(Char.IsLower(str_to_array[i]) == true)
+                {
+                    str_to_array[i] = Char.ToUpper(str_to_array[i]);
+                }
+            }
+
+            // save reversed char to string
+            str = new string(str_to_array);
+
+            // print the reverse string
+            Console.WriteLine(str);
+
+            Console.ReadLine(); // click to close
+        }
+    }
+}
